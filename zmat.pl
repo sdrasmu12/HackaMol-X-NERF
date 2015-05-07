b29ce8078599dd39632ac4bf8e9d5d46705da722
Demian Riccardi <demianriccardi@gmail.com>
2015-05-07 09:20:14 -0400

    improving zmat reader

--- zmat.pl
+++ zmat.pl
@@ -1,6 +1,8 @@
 use Modern::Perl;
 use HackaMol::X::NERF;
+use Math::Vector::Real;
 use YAML::XS;
+use Scalar::Util qw(looks_like_number);
 
 my @zmat = <DATA>;
 chomp @zmat;
@@ -11,14 +13,16 @@ my %mol;
 
 foreach my $iatom (0 .. $#atms){
   # check if it's cartesian
-  my ($sym, $iat1, $dist, $iat2, $ang, $iat3, $tors) = split (/ /, $atom);
+  my ($sym, $iat1, $dist, $iat2, $ang, $iat3, $tors) = split (/ /, $atms[$iatom]);
   if ( defined($iat1) ){
-    if ( $iat == 0 ){
-      $mol{atoms}[$iatom] = {
+    if ( looks_like_number($iat1)){ 
+      if ( $iat1 == 0 ){
+        $mol{atoms}[$iatom] = {
                               symbol  =>  $sym , 
                               coords => [ V($dist, $iat2, $ang) ]
-                            }
-      say $sym,' ', $dist;
+                            };
+        print Dump \%mol;
+      }
     }
   } 
   
@@ -38,6 +42,7 @@ C 1 R1
 C  2 R1 1 A1
 C 3 R1 2 A1 3 D1
 C 4 R1 3 A1 2 D1
+C 0 1 1 1
 R1 1.5
 A1 109
 D1 180.0
