c46ff98ed2002ade861a41367b1ad2d65ca126d0
Demian Riccardi <demianriccardi@gmail.com>
2015-05-19 10:04:29 -0400

    getting ready for Zmatrices

--- lib/HackaMol/X/NERF.pm
+++ lib/HackaMol/X/NERF.pm
@@ -6,51 +6,7 @@ use Moo;
 use Math::Vector::Real;
 use Math::Trig; 
 
-my $orig = V(0,0,0);
-
-sub init {
-  # adding the first vector
-  my $self = shift;
-  if (@_ == 3){
-    return V(@_);
-  }
-  else {
-    return $orig;
-  }
-}
-
-
-sub extend_a {
-  #allow the use of optvec to give control over the addition
-  my ($self, $a, $R, $optvec) = @_;
-  $optvec = V(1,0,0) unless defined($optvec);
-  return ($a + $R*$optvec->versor); 
-}
-
-sub extend_ab {
-  my ($self,$a,$b,$R,$ang) = @_;
-  $ang  = deg2rad(180-$ang);
-  my ($ba, $j, $k) = ($b-$a)->rotation_base_3d;
-  my $c = $b+$ba*$R;
-  $c = $j->rotate_3d($ang, $c-$b) + $b;
-  return ($c);
-}
-
-sub extend_abc {
-  my ($self,$a,$b,$c,$R,$ang,$tors) = @_;
-  $ang  = deg2rad(180-$ang);
-  $tors = deg2rad($tors);
-  my $cang = cos($ang);
-  my $sang = sin($ang);
-  my $ctor = cos($tors);
-  my $stor = sin($tors);
-
-  my $bc = ($c-$b)->versor;
-  my $n = (($b-$a) x $bc)->versor;
-
-  my $D = $R*($bc*$cang + ($n x $bc)*$sang*$ctor + $n*$sang*$stor) + $c;
-  return $D;
-}
+with 'HackaMol::Roles::NERFRole';
 
 1;
 
